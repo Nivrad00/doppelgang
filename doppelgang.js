13f5389e723479b8a2ff0efbac9328cc89eb2db3
Nivrad00 <darvinheo@gmail.com>
2018-08-20 14:23:11 -0400

    Update doppelgang.js

--- doppelgang.js
+++ doppelgang.js
@@ -2,13 +2,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
-
 
 client.on('message', message => {
   if (message.content == "doppel")
     message.reply("gang!");
 }
+
+client.login(); // key goes here
