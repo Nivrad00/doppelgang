0ec381c1b0ac89a831f67858d65d770740eb5fe7
Nivrad00 <darvinheo@gmail.com>
2018-08-24 00:23:56 -0400

    readme changes

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ DoppelGang requires the Manage Channels and Manage Roles permissions, and the ab
 
 **Overview**
 
-During a round of DoppelGang, players communicate anonymously with each other by sending the bot DMs, which are transferred anonymously to the gameplay channel. Each player is identified only by the color of their messages. One player is the doppelganger, and the rest are adventurers. The adventurers must figure out which player is the doppelganger, then collectively vote to kill them. The doppelganger's goal is to survive by impersonating one of the adventurers, tricking them into killing one of their own.
+During a round of DoppelGang, players communicate anonymously with each other by sending the bot DMs, which are transferred to the gameplay channel with only a color to identify the sender. One player is the doppelganger, and the rest are adventurers. The adventurers must figure out which player is the doppelganger, then collectively vote to kill them. The doppelganger's goal is to survive by impersonating one of the adventurers, tricking them into killing one of their own.
 
 **Setup**
 
--- game.js
+++ game.js
@@ -31,7 +31,7 @@ class Game {
             'DoppelGang requires the Manage Channels and Manage Roles permissions, and the ability to send DMs to the players.',
             '',
             '**Overview**',
-            'During a round of DoppelGang, players communicate anonymously with each other by sending the bot DMs, which are transferred anonymously to the gameplay channel. Each player is identified only by the color of their messages. One player is the doppelganger, and the rest are adventurers. The adventurers must figure out which player is the doppelganger, then collectively vote to kill them. The doppelganger\'s goal is to survive by impersonating one of the adventurers, tricking them into killing one of their own.',
+            'During a round of DoppelGang, players communicate anonymously with each other by sending the bot DMs, which are transferred to the gameplay channel with only a color to identify the sender. One player is the doppelganger, and the rest are adventurers. The adventurers must figure out which player is the doppelganger, then collectively vote to kill them. The doppelganger\'s goal is to survive by impersonating one of the adventurers, tricking them into killing one of their own.',
             '',
             '**Setup**',
             'Players can join and leave the game using the "leave" and "join" commands. Once the party leader starts the round with the "ready" command, each player is asked via DM to set a preference for doppelganger or adventurer. The bot automatically assigns the players their roles and colors, then creates the gameplay channel and starts the round.',
